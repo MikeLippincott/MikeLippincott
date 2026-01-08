db4eff3288e680f762237c8e11ed43ff952a4bdc
MikeLippincott <1michaell2017@gmail.com>
2026-01-08 10:01:58 -0700

    CV builder

--- CV_assemble/build_cv.sh
+++ CV_assemble/build_cv.sh
@@ -8,4 +8,12 @@ pdflatex -interaction=nonstopmode CV.md
 # Clean up auxiliary files
 rm -f CV.aux CV.log CV.out
 
+current_year=$(date +%Y)
+mv CV.pdf ../Michael_J_Lippincott_CV_${current_year}.pdf
+
+# remove previous year's CV
+if [ -f ../Michael_J_Lippincott_CV_$((current_year - 1)).pdf ]; then
+    rm ../Michael_J_Lippincott_CV_$((current_year - 1)).pdf
+fi
+
 echo "PDF built successfully: CV.pdf"

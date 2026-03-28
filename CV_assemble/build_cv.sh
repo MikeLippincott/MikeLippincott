44fe8942f5142caec39747a49b65eb1de5bfe7d2
MikeLippincott <1michaell2017@gmail.com>
2026-03-28 13:49:50 -0600

    update build

--- CV_assemble/build_cv.sh
+++ CV_assemble/build_cv.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+rm ../Michael_J_Lippincott_CV_*.pdf
+
 # generate the tex file from toml file
 python build_tex_cv.py --input "cv_profile.toml" --output "cv.tex"
 
@@ -10,11 +12,9 @@ pdflatex -interaction=nonstopmode cv.tex
 rm cv.aux cv.log cv.out
 
 current_year=$(date +%Y)
-current_year=$(date +%Y)
-mv cv.pdf ../Michael_J_Lippincott_CV_${current_year}.pdf
+current_month=$(date +%m)
+mv cv.pdf ../Michael_J_Lippincott_CV_${current_year}_${current_month}.pdf
 # remove previous year's CV
-if [ -f ../Michael_J_Lippincott_CV_$((current_year - 1)).pdf ]; then
-    rm ../Michael_J_Lippincott_CV_$((current_year - 1)).pdf
-fi
 
-echo "PDF built successfully: Michael_J_Lippincott_CV_${current_year}.pdf"
+
+echo "PDF built successfully: Michael_J_Lippincott_CV_${current_year}_${current_month}.pdf"
